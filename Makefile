c6d85db9fb7bd902e155618b5ede9addeb443484
vendethiel <vendethiel@hotmail.fr>
2016-03-17 22:19:18 +0100

    first step

---
+++ Makefile
@@ -0,0 +1,6 @@
+PROJECT = ferl
+PROJECT_DESCRIPTION = Restful+Erlang
+PROJECT_VERSION = 0.0.1
+DEPS = cowboy
+
+include erlang.mk
---
+++ src/ferl_app.erl
@@ -0,0 +1,18 @@
+-module(ferl_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+  Dispatch = cowboy_router:compile([
+    {'_', [{"/", root_handler, []}]}
+  ]),
+  {ok, _} = cowboy:start_http(ferl_app, 100,
+    [{port, 8181}],
+    [{env, [{dispatch, Dispatch}]}]
+  ),
+	ferl_sup:start_link().
+
+stop(_State) ->
+	ok.
